e80b17f36f5fc8f9f8c16a23077ec24c7feda186
AlexNS <alex.shkurko@gmail.com>
2025-07-09 15:03:28 +0300

    Update function name

--- custom_components/mega/__init__.py
+++ custom_components/mega/__init__.py
@@ -213,7 +213,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await hub.start()
     for platform in PLATFORMS:
         hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(
+            hass.config_entries.async_forward_entry_setups(
                 entry, platform
             )
         )
